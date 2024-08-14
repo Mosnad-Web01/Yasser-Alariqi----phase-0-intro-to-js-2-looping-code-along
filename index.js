cd4c839a8caccc907c38a8c7ed76399fc3a6f93a
Yasser Esam <yaaser10esam1010@gmail.com>
2024-08-14 14:05:47 +0300

    adding js code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, gift) {
+    let myGreetingArray = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        myGreetingArray.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+    }
+    return myGreetingArray;
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number--
+    }
+}
+
+countDown(10)
